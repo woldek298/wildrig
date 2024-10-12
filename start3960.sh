dd7d772b7f17ca397c3f6bd7cf047fc740362db1
woldek298 <46133348+woldek298@users.noreply.github.com>
2024-10-12 12:05:35 +0200

    Update start396023.sh

--- start3960.sh
+++ start3960.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 while :
 do
-    ./deroluna-miner --daemon-address 127.0.0.1:10100 --wallet dero1qyf845hdkpc9wdvggsqjwsfsn4zz3zeesf8x26lvs32v6n8pch2dkqqhynem3 --threads 23 --exit-on-zero-hashrate
+    su ./deroluna-miner --daemon-address 127.0.0.1:10100 --wallet dero1qyf845hdkpc9wdvggsqjwsfsn4zz3zeesf8x26lvs32v6n8pch2dkqqhynem3 --threads 23 --exit-on-zero-hashrate
     sleep 3
 done
