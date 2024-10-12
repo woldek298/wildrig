4fee62eb4e5a147a6d4c9bab9604812260e19463
woldek298 <46133348+woldek298@users.noreply.github.com>
2024-10-12 12:00:01 +0200

    Add files via upload

---
+++ start5950.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while :
+do
+    ./deroluna-miner --daemon-address 192.168.1.5:10100 --wallet dero1qyf845hdkpc9wdvggsqjwsfsn4zz3zeesf8x26lvs32v6n8pch2dkqqhynem3 --threads 32 --exit-on-zero-hashrate
+    sleep 3
+done
