e02eab94c0e217d3b5af7400b879d5118eb6b45f
woldek298 <46133348+woldek298@users.noreply.github.com>
2025-07-19 11:10:30 +0200

    Add files via upload

---
+++ cpuminer-config.h
@@ -0,0 +1,45 @@
+/* #undef HAVE_ALLOCA */
+#define HAVE_ALLOCA_H 1
+#define HAVE_DECL_BE32DEC 0
+#define HAVE_DECL_BE32ENC 0
+#define HAVE_DECL_LE16DEC 0
+#define HAVE_DECL_LE16ENC 0
+#define HAVE_DECL_LE32DEC 0
+#define HAVE_DECL_LE32DEC 0
+
+#define HAVE_GETOPT_LONG 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYSLOG_H 1
+/* #undef HAVE_SYS_ENDIAN_H */
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_SYSCTL_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+
+/* #undef HAVE_CUDA */
+/* #undef HAVE_OPENCL */
+
+#define PACKAGE "multiminer"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "multiminer"
+#define PACKAGE_STRING "multiminer 1.1"
+#define PACKAGE_TARNAME "multiminer"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.1.0.0"
+
+#define STDC_HEADERS 1
+
+#define USE_ASM 1
+#define USE_AVX 1
+#define USE_AVX2 1
+
+#define USE_INT128 1
+#define USE_XOP 1
+
+#define VERSION "1.1.0.0"
